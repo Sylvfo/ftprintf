4ea1269907d01c0d16db7d09ee5cb465a7620d83
Sylvie Forster <sforster@c1r7s3.42lausanne.ch>
2023-11-14 11:39:42 +0100

    jeudi 14 novmatinecole

---
+++ test.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sforster <sforster@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 08:50:30 by sforster          #+#    #+#             */
+/*   Updated: 2023/11/14 11:38:23 by sforster         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("I was in Paris this weekend for 4 days\n");
+	printf("I was in %s this weekend for %d days\n", "Paris", 4);
+}
